0001bde3c84a17fcbe9602987e8ca05cdb174296
Maksym Chernii <mch.maximcherniy@gmail.com>
2022-02-21 14:01:08 -0800

    refactor: replace asserts to Assertions class method.

--- tests/test_users_auth.py
+++ tests/test_users_auth.py
@@ -1,8 +1,11 @@
 import pytest
 import requests
 
+from utils.base_case import BaseCase
+from utils.assertions import Assertions
 
-class TestUsersAuth:
+
+class TestUsersAuth(BaseCase):
     exclude_params = [
         ("no_cookie"),
         ("no_token")
@@ -18,24 +21,22 @@ class TestUsersAuth:
         }
 
         response = requests.post(url=self.login_url, data=data)
-        assert "auth_sid" in response.cookies, "There is no auth cookie in the response."
-        assert "x-csrf-token" in response.headers, "There is no CSRF token header in the response."
-        assert "user_id" in response.json(), "There is no user id in the response."
 
-        self.auth_sid = response.cookies.get("auth_sid")
-        self.token = response.headers.get("x-csrf-token")
-        self.user_id_from_auth_endpoint = response.json()["user_id"]
+        self.auth_sid = self.get_cookie(response=response, cookie_name="auth_sid")
+        self.token = self.get_header(response=response, headers_name="x-csrf-token")
+        self.user_id_from_auth_endpoint = self.get_json_value(response=response, name="user_id")
+
         self.headers = {"x-csrf-token": self.token}
         self.cookies = {"auth_sid": self.auth_sid}
 
     def test_auth_user(self):
         response = requests.get(url=self.check_user_auth_url, headers=self.headers, cookies=self.cookies)
-
-        assert "user_id" in response.json(), "There is no user_id in the second response."
-        user_id_from_check_endpoint = response.json()["user_id"]
-
-        assert self.user_id_from_auth_endpoint == user_id_from_check_endpoint, \
-            "User id from auth endpoint is not equal to User id from check endpoint."
+        Assertions.assert_json_value_by_name(
+            response=response,
+            name="user_id",
+            expected_value=self.user_id_from_auth_endpoint,
+            error_message="User id from auth endpoint is not equal to user id from check endpoint"
+        )
 
     @pytest.mark.parametrize("condition", exclude_params)
     def test_negative_auth_check(self, condition):
@@ -43,7 +44,9 @@ class TestUsersAuth:
             requests.get(url=self.check_user_auth_url, headers=self.headers) if condition == "no_cookie" \
             else requests.get(url=self.check_user_auth_url, cookies=self.cookies)
 
-        assert "user_id" in response.json(), "There is no user id in the second response."
-
-        user_id_from_check_endpoint = response.json()["user_id"]
-        assert user_id_from_check_endpoint == 0, f"User is authorize with condition {condition}."
+        Assertions.assert_json_value_by_name(
+            response=response,
+            name="user_id",
+            expected_value=0,
+            error_message=f"User is authorize with condition {condition}."
+        )
